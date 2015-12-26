870f558cf1fd5e6bff51dc4914ec4506105de940
Garik Harutyunyan <garik.harutyunyan99@gmail.com>
2015-12-26 19:40:52 +0400

    erkrort commit

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => 'car.pe.hu',
+            'database'  => 'u719192340_blog',
+            'username'  => 'u719192340_hayko',
+            'password'  => 'hayko009',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
